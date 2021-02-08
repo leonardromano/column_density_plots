c07cff85320da5d1c68c3f23b4c48786e154ed9a
leonardromano <leonard.romano@tum.de>
2021-02-08 15:41:49 +0100

    cleaned up the documentation

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # column_density_plots
-A script for making column density plots
+A parallel python script for making column density plots
+
+# non-standard dependencies
+The code makes use of the following two non-standard libraries:
+- psutil
+- ray
+
+using pip they can easily be installed:
+
+	pip install psutil ray
+ 
 
 # usage
-The script is self contained and meant to be imported in your analysis code. 
+The latest version of the code uses ray for parallelization. If this code is embedded in a larger framework it is probably advisable to comment out the lines ray.init() and ray.shutdown() from the code and put them at the beginning and end of your code in order to avoid multiple (cosly) reinitializations of ray or possible even crashes with already existing instances of ray!
+Further you need to specify which scheduler you are using if you are working on a cluster, such that ray can assign the correct number of CPUs. If nothing is specified, it will simply take all physical CPUs on the machine.
+
+Other than the above mentioned configuration the script is self contained and meant to be imported in your analysis code. 
 The main routine for making column density plots is smoothColumnDensityPlot so probably an import like
 
 "from column_density import smoothColumnDensityPlot"
@@ -13,5 +26,5 @@ smoothColumnDensityPlot takes as arguments:
 (numpy_array (N,3) positions, numpy_array (N) smoothingLengths, numpy_array (N) densities, numpy_array (N) masses, 
  int NumberOfBins (default=100), float boxlength (default=15.(code units)), string colormap (default="inferno"), string output_directory (default="./")) 
 
-By default the code assumes that you are plotting a disk rotating around the z-axis. You need to align your data in such a way first before calling this code, in order to get a useful result. The code makes use of the "cubic B-spline" for its computation of the SPH densities. If you are using a different Kernel function, feel free to modify W(x,h) to your Kernel function -- the modifications should be straightforward.
+By default the code assumes that you are plotting a disk rotating around the z-axis. It then constructs and plots the 2D-density maps for a box enclosing the region (-boxlength, boxlength)^3, so in order to make use of it, it will be necessary to align the coordinates accordingly. The code makes use of the "cubic B-spline" for its computation of the SPH densities. If you are using a different Kernel function, feel free to modify W(x,h) to your Kernel function -- the modifications should be straightforward.
 If you have any suggestions for extra functionalities feel free to modify the code and contact me, if you think your modifications should be shared with the world!
--- column_density.py
+++ column_density.py
@@ -8,19 +8,20 @@ Created on Fri Jan 22 13:20:35 2021
 from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt
 from numpy import array, zeros, sqrt, pi, linspace, meshgrid
-from  os import environ
+from os import environ
 from psutil import cpu_count
 import ray
 from time import time
 
-
-#################################
+###############################################################################
 #global variables
 #proton mass
 mp = 1.672621637*10**(-24)
+TINY_NUMBER = 1e-20
+
 #depending on the used scheduling system we have a
 #different environment variable for the number of available cores
-Scheduler = "SGE"
+Scheduler = ""
 if Scheduler == "SGE":
     NCPU = int(environ['NSLOTS'])
 elif Scheduler == "SLURM":
@@ -29,38 +30,29 @@ elif Scheduler == "PBS":
     NCPU = int(environ['PBS_NP'])
 else:
     NCPU = cpu_count(logical=False)
-TINY_NUMBER = 1e-20
-#################################
 
-#call this function to make a column density plot
-def smoothColumnDensityPlot(positions, smoothingLengths, densities, masses, \
-                            Nbins = 100, boxlength = 15, \
-                            cmap = "inferno", output_dir = "./"):
-    "Makes a Column Density plot including smoothing length"
-    #first compute column densities
-    ray.shutdown()
-    ray.init(num_cpus = NCPU)
-    print("Making column density plot using %d processor cores."%NCPU)
-    particles = array([Particle(positions[i], smoothingLengths[i], \
-                          densities[i], masses[i]) \
-                 for i in range(positions.shape[0])])
-    density_xz, density_xy = densityDistribution(particles, Nbins, boxlength)
-    ray.shutdown()
-    #create grid
-    XX = linspace(-boxlength, boxlength, Nbins)
-    YY = linspace(-boxlength, boxlength, Nbins)
-    X,Y = meshgrid(XX,YY)
-    #finally make the plots
-    t0 = time()
-    makePlot(X,Y, density_xy, cmap, "face on", output_dir)
-    makePlot(X,Y, density_xz, cmap, "edge on", output_dir)
-    t1 = time()
-    print("finished making column density plots! Took %g seconds." %(t1-t0))
 
-#a class for handling the parallel part of the density computation
+###############################################################################
+#SPH Kernel: Adjust to your liking
+def W(q, h):
+    "cubic B4-spline"
+    if 0 <= q <=0.5:
+        return (1 - 6 * q**2 + 6 * q**3) / (pi * h**3)
+    elif 0.5 <= q <= 1:
+        return (2 * (1 - q)**3) * 8 / (pi * h**3)
+    elif 1 < q:
+        return 0
+
+###############################################################################
+#worker class
+
 @ray.remote
-class Density_data():
-    def __init__(self, binNumber, boxsize):
+class worker():
+    def __init__(self, particles_ref, ID, load, binNumber, boxsize):
+        if ID < NCPU-1:
+            self.particles = particles_ref[ID * load:(ID+1) * load]
+        else:
+            self.particles = particles_ref[ID * load:]
         self.binNumber = binNumber
         self.boxsize = boxsize
         self.dr = 2 * boxsize / binNumber
@@ -118,11 +110,10 @@ class Density_data():
                     #add particles contribution to volume element weight in cell
                     self.w_3d[i,j,k] += mlwk/p.Density
                     
-    def process_particles(self, particles):
-        for particle in particles:
+    def process(self):
+        for particle in self.particles:
             self.update(particle)
         return self.rho_3d, self.w_3d
-        
 
 #this class stores the particle data
 class Particle():
@@ -132,40 +123,77 @@ class Particle():
         self.Density = density
         self.Mass = mass
 
-#This function can be replaced with your SPH Kernel function of choice
-def W(q, h):
-    "cubic B4-spline"
-    if 0 <= q <=0.5:
-        return (1 - 6 * q**2 + 6 * q**3) / (pi * h**3)
-    elif 0.5 <= q <= 1:
-        return (2 * (1 - q)**3) * 8 / (pi * h**3)
-    elif 1 < q:
-        return 0
+###############################################################################
+# Functions for plotting
 
 def readable(label):
+    "Emboldens labels"
     return r"$\mathrm{\mathbf{" + label + "}}$"
 
-def densityDistribution(particles, binNumber = 100, boxsize = 15):
+def makePlot(X, Y, data, cmap, mode, output_dir):
+    "Makes a colormesh plot and saves it to output_dir"
+    plt.rcParams["font.size"] = 14
+    plt.pcolormesh(X, Y, data, cmap = cmap, norm = LogNorm(), shading = 'gouraud')
+    plt.colorbar(label='column gas mass density')
+    plt.title("Density distribution of gas (" +mode+")")
+    plt.xlabel(readable("X [kpc]"))
+    if mode == "edge on":
+        plt.ylabel(readable("Z [kpc]"))
+    else:
+        plt.ylabel(readable("Y [kpc]"))
+    plt.savefig(output_dir + "column_density_"+ mode +".pdf", bbox_inches='tight')
+    plt.close()
+    plt.rcParams["font.size"] = 10
+    
+###############################################################################
+
+#call this function to make a column density plot
+def smoothColumnDensityPlot(positions, smoothingLengths, densities, masses, \
+                            Nbins = 100, boxlength = 15, cmap = "inferno", \
+                            output_dir = "./"):
+    "Makes a Column Density plot including smoothing length"
+    #first compute column densities
+    ray.init(num_cpus = NCPU)
+    print("Making column density plot using %d processor cores."%NCPU)
+    Npart = smoothingLengths.shape[0]
+    particles_ref = ray.put(array([Particle(positions[i], smoothingLengths[i], \
+                                        densities[i], masses[i]) \
+                               for i in range(positions.shape[0])]))
+    density_xz, density_xy = densityDistribution(particles_ref, Npart, \
+                                                 Nbins, boxlength)
+    ray.shutdown()
+    #create grid
+    XX = linspace(-boxlength, boxlength, Nbins)
+    YY = linspace(-boxlength, boxlength, Nbins)
+    X,Y = meshgrid(XX,YY)
+    #finally make the plots
+    t0 = time()
+    makePlot(X,Y, density_xy, cmap, "face on", output_dir)
+    makePlot(X,Y, density_xz, cmap, "edge on", output_dir)
+    t1 = time()
+    print("finished making column density plots! Took %g seconds." %(t1-t0))
+        
+###############################################################################
+# Function for density computation
+
+def densityDistribution(particles_ref, Npart, binNumber = 100, boxsize = 15):
     """calculates the column density including the smoothing length
     for gas particles"""
-    #initialize arrays for data storage
-    actors = [Density_data.remote(binNumber, boxsize) for _ in range(NCPU)]
-    rho_3d = zeros((binNumber,binNumber,binNumber))
-    w_3d   = zeros((binNumber,binNumber,binNumber))
-    rho_xz = zeros((binNumber,binNumber))
-    rho_xy = zeros((binNumber,binNumber))
     
     t0 = time()
     print("calculating spatial density...\n")
-    #first loop over all particles to compute densities and weights
+    
+    #first loop over all particles computing densities and weights
     #spread the work evenly among all processors
-    n = len(particles)//NCPU
-    result_ids = [actors[i].process_particles.remote(particles[i * n : (i + 1) * n]) \
-                  for i in range(NCPU-1)]
-        
-    result_ids.append(actors[NCPU-1].process_particles.\
-                      remote(particles[(NCPU-1) * n :]))
-    #now combine the contributions from all processors
+    load = Npart//NCPU
+    actors = [worker.remote(particles_ref, i, load, binNumber, boxsize) \
+              for i in range(NCPU)]
+    
+    result_ids = [actor.process.remote() for actor in actors]
+    
+    #now reduce the individual results
+    rho_3d = zeros((binNumber,binNumber,binNumber))
+    w_3d   = zeros((binNumber,binNumber,binNumber))
     while len(result_ids):
         done_id, result_ids = ray.wait(result_ids)
         drho_3d, dw_3d = ray.get(done_id[0])
@@ -178,7 +206,9 @@ def densityDistribution(particles, binNumber = 100, boxsize = 15):
     #weight the densities with the SPH-Volume
     rho_3d /= (w_3d + TINY_NUMBER)
         
-    #now integrate over the line of sights
+    #Integrate over the lines of sight
+    rho_xz = zeros((binNumber,binNumber))
+    rho_xy = zeros((binNumber,binNumber))
     for i in range(binNumber):
         for j in range(binNumber):
             for k in range(binNumber):
@@ -192,20 +222,3 @@ def densityDistribution(particles, binNumber = 100, boxsize = 15):
     t4 = time()
     print("finished calculating the spatial density! Took %g seconds" %(t4-t0))
     return rho_xz, rho_xy
-
-def makePlot(X, Y, data, cmap, mode, output_dir):
-    "Makes a colormesh plot and optionally saves it"
-    plt.rcParams["font.size"] = 14
-    plt.pcolormesh(X, Y, data, cmap = cmap, \
-                   norm = LogNorm(), shading = 'gouraud')
-    plt.colorbar(label='column gas mass density')
-    plt.title("Density distribution of gas (" +mode+")")
-    plt.xlabel(readable("X [kpc]"))
-    if mode == "edge on":
-        plt.ylabel(readable("Z [kpc]"))
-    else:
-        plt.ylabel(readable("Y [kpc]"))
-    plt.savefig(output_dir + "column_density_"+ mode +".pdf", bbox_inches='tight')
-    plt.show()
-    plt.close()
-    plt.rcParams["font.size"] = 10
